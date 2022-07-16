d462d6b8674ff9a4dae86b7efb02bb3d41983f6e
Matricarin <matricarin@gmail.com>
2022-07-16 15:54:50 +0500

    First BeamApp!

---
+++ BeamApp/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures;
+using Tekla.Structures.Model;
+using TSG = Tekla.Structures.Geometry3d;
+
+namespace BeamApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BeamButton_Click(object sender, EventArgs e)
+        {
+            Model myModel = new Model();
+            if (myModel.GetConnectionStatus())
+            {
+                TSG.Point startPoint = new TSG.Point(0, 0, 0);
+                TSG.Point endPoint = new TSG.Point(0, 3000, 0);
+                Beam myBeam = new Beam();
+            }
+        }
+    }
+}
---
+++ BeamApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures;
+using Tekla.Structures.Model;
+using TSG = Tekla.Structures.Geometry3d;
+
+namespace BeamApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }        
+    }
+}
